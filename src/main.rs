681f868be76a85914b24d56729484584f20c97b1
brandly <brandly13@gmail.com>
2021-12-31 23:22:09 -0500

    6.1

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,10 @@ fn write_color(pixel_color: Vec3) {
 }
 
 fn ray_color(ray: Ray) -> Vec3 {
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let normal = unit_vector(ray.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Vec3::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0)
     }
     let unit_direction = unit_vector(ray.direction);
     let t = 0.5 * (unit_direction.y() + 1.0);
@@ -23,13 +25,17 @@ fn ray_color(ray: Ray) -> Vec3 {
 // center + radius is the sphere
 // does ray hit it?
 // define quadratic and solve
-fn hit_sphere(center: Vec3, radius: f32, ray: Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f32, ray: Ray) -> f32 {
     let oc = ray.origin - center;
     let a = ray.direction.dot(ray.direction);
     let b = 2.0 * oc.dot(ray.direction);
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn main() {
